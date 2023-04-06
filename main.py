220f0160232e953b1728bf1da813ae1bf6ab0c49
BtNowakowski <107316656+BtNowakowski@users.noreply.github.com>
2023-04-06 15:46:51 +0200

    *

--- main.py
+++ main.py
@@ -21,17 +21,13 @@ def create_filter():
     return filter
 
 
-def create_query():
+def get_query():
     query = sys.argv[1:]
     return " ".join(query)
 
 
-def create_url():
-    if len(sys.argv[1:]) == 0:
-        print("Error, not a valid query")
-    else:
-        final_url = url + create_query() + " " + create_filter()
-        webbrowser.open(final_url)
-
-
-create_url()
+if len(sys.argv[1:]) == 0:
+    print("Error, not a valid query")
+else:
+    final_url = url + get_query() + " " + create_filter()
+    webbrowser.open(final_url)
